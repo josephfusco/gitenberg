b823a0768dc1270328e7fbb0f86a18cdf6d86386
Joe Fusco <joe.fusco@wpengine.com>
2023-12-05 21:45:10 -0500

    improve testability
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- includes/admin/functions.php
+++ includes/admin/functions.php
@@ -8,16 +8,23 @@
 namespace Gitenberg\Admin;
 
 /**
+ * HTML template for admin notice about missing GitHub credentials.
+ */
+function get_admin_notice_html() {
+    return '
+        <div class="notice notice-error">
+            <p>
+                The <code>GITENBERG_GITHUB_REPO</code> and <code>GITENBERG_GITHUB_PERSONAL_ACCESS_TOKEN</code> are required for the <strong>Gitenberg</strong> plugin to function properly. Please define them in your wp-config.php file or set them in the WordPress options.
+            </p>
+        </div>
+    ';
+}
+
+/**
  * Displays an admin notice about missing GitHub credentials.
  */
 function display_admin_notice() {
-    ?>
-    <div class="notice notice-error">
-        <p>
-            The <code>GITENBERG_GITHUB_REPO</code> and <code>GITENBERG_GITHUB_PERSONAL_ACCESS_TOKEN</code> are required for the <strong>Gitenberg</strong> plugin to function properly. Please define them in your wp-config.php file or set them in the WordPress options.
-        </p>
-    </div>
-    <?php
+    echo get_admin_notice_html();
 }
 
 /**
@@ -28,41 +35,22 @@ function display_admin_notice() {
  */
 function get_linked_markdown_file( $post_id ) {
     if ( ! get_post_status( $post_id ) ) {
-        // Post does not exist
-        return null;
+        return null; // Post does not exist
     }
 
-    $post_meta = get_post_meta( $post_id, 'gitenberg_linked_markdown_file', true );
-
-    if ( empty( $post_meta ) ) {
-        // Post meta is empty or does not exist
-        return null;
-    }
-
-    return $post_meta;
+    return get_post_meta( $post_id, 'gitenberg_linked_markdown_file', true ) ?: null;
 }
 
 /**
  * Get the SHA (Secure Hash Algorithm) of the linked GitHub markdown file for a post.
  *
- * This function retrieves the SHA (Secure Hash Algorithm) of the GitHub markdown file associated with a post.
- *
  * @param int $post_id The ID of the post.
- *
  * @return string|null The SHA of the linked markdown file, or null if not found or invalid post ID.
  */
 function get_linked_markdown_file_sha( $post_id ) {
     if ( ! get_post_status( $post_id ) ) {
-        // Post does not exist
-        return null;
+        return null; // Post does not exist
     }
 
-    $sha = get_post_meta( $post_id, 'gitenberg_linked_markdown_file_sha', true );
-
-    if ( empty( $sha ) ) {
-        // Sha is empty or does not exist
-        return null;
-    }
-
-    return $sha;
+    return get_post_meta( $post_id, 'gitenberg_linked_markdown_file_sha', true ) ?: null;
 }
